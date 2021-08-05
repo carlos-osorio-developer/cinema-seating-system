8cdba35ce2d8bc016079a87ec3f13a2618cb558c
carlos-osorio-developer <osorio.uis@gmail.com>
2021-08-05 11:59:47 -0500

    Initial commit with title and h1 tag
    Signed-off-by: carlos-osorio-developer <osorio.uis@gmail.com>

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Reserva tu silla</title>
+    </head>
+    <body>
+        <main style="display: flex; flex-direction: column; align-items: center;">
+            <h1>¡Reserva tu silla!</h1>
+        </main>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
