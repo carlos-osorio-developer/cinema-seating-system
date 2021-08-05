f6bbd1c7c71ce776dc9f090aca1bab4c4f8c936a
carlos-osorio-developer <osorio.uis@gmail.com>
2021-08-05 14:31:56 -0500

    Add second font and description on website

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Reserva tu silla</title>
+        <title>Sistema de reservas</title>
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Merienda:wght@400;700&display=swap" rel="stylesheet">
@@ -15,7 +15,8 @@ and open the template in the editor.
     </head>
     <body>
         <main>
-            <h1 class="titles">¡Reserva tu silla!</h1>
+            <h1 class="titles">¡Reserva tu asiento!</h1>
+            <p>En este sitio puedes reservar tus asientos preferidos.</p>            
         </main>
         <?php
         // put your code here
